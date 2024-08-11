953dea901469f5be99d297312098cd9b1184d4d2
ShivamVatsAs <shivamchy867@gmail.com>
2024-08-11 16:44:02 +0530

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,4 +1,4 @@
-const anniversaryDate = 'June 02, 2024 00:00:00'
+const anniversaryDate = 'June 29, 2024 00:00:00'
 const noMessage = 'With you, even the ordinary becomes extraordinary. The days we spend together are a constant reminder of the beauty that unfolds when two souls connect.'
 const endMessage = '<br><br><br>Since June 2nd, 2024, we have embarked on a remarkable journey together. You are my partner, my confidante, and the love of my life. Here is to endless adventures and a lifetime of love,<br>AShivii❤️'
 const messages = {
